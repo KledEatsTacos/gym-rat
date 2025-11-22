f8a142d0aa18f8e016f52eb8f9c358e4c65a235c
Mustafa Masri <mustafamasry180@gmail.com>
2025-11-22 15:51:39 +0300

    Add complete Razor Pages solution for gym-rat project

---
+++ gym-rat/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace gym_rat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
