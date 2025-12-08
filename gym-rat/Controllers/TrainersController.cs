b00d51e534e273a030207ec14def48ce2ea5e822
Mustafa Masri <mustafamasry180@gmail.com>
2025-12-08 17:38:47 +0300

    trainer CRUD operations

--- gym-rat/Controllers/TrainersController.cs
+++ gym-rat/Controllers/TrainersController.cs
@@ -1,6 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using gym_rat.Data;
+using gym_rat.Models;
 
 namespace gym_rat.Controllers
 {
@@ -13,9 +19,142 @@ namespace gym_rat.Controllers
             _context = context;
         }
 
+        // GET: Trainers
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Trainers.ToListAsync());
+            var trainers = await _context.Trainers.Include(t => t.Gym).ToListAsync();
+            return View(trainers);
+        }
+
+        // GET: Trainers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trainer = await _context.Trainers
+                .Include(t => t.Gym)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (trainer == null)
+            {
+                return NotFound();
+            }
+
+            return View(trainer);
+        }
+
+        // GET: Trainers/Create
+        public IActionResult Create()
+        {
+            ViewData["GymId"] = new SelectList(_context.Gyms, "Id", "Name");
+            return View();
+        }
+
+        // POST: Trainers/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Specialization,StartTime,EndTime,GymId")] Trainer trainer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(trainer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GymId"] = new SelectList(_context.Gyms, "Id", "Name", trainer.GymId);
+            return View(trainer);
+        }
+
+        // GET: Trainers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trainer = await _context.Trainers.FindAsync(id);
+            if (trainer == null)
+            {
+                return NotFound();
+            }
+            ViewData["GymId"] = new SelectList(_context.Gyms, "Id", "Name", trainer.GymId);
+            return View(trainer);
+        }
+
+        // POST: Trainers/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Specialization,StartTime,EndTime,GymId")] Trainer trainer)
+        {
+            if (id != trainer.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(trainer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TrainerExists(trainer.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GymId"] = new SelectList(_context.Gyms, "Id", "Name", trainer.GymId);
+            return View(trainer);
+        }
+
+        // GET: Trainers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trainer = await _context.Trainers
+                .Include(t => t.Gym)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (trainer == null)
+            {
+                return NotFound();
+            }
+
+            return View(trainer);
+        }
+
+        // POST: Trainers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var trainer = await _context.Trainers.FindAsync(id);
+            if (trainer != null)
+            {
+                _context.Trainers.Remove(trainer);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TrainerExists(int id)
+        {
+            return _context.Trainers.Any(e => e.Id == id);
         }
     }
 }
